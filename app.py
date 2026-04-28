63ac7c505b8e5ee17aae3b55e3f6b531bb2fe009
ParitYadav <2022ucp1919@mnit.ac.in>
2026-04-29 04:51:27 +0530

    fix: add tensorflow import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
